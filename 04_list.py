b4919b651e6e68dbae5391d114ad16c6790b1b78
tutankadev <kenyihq@gmail.com>
2024-05-31 14:57:46 -0500

    Added lists

---
+++ 04_list.py
@@ -0,0 +1,16 @@
+#normal
+
+numbers = []
+
+for element in range(1, 11):
+    numbers.append(element)
+
+
+print(numbers)
+
+# list_comprenhension
+
+
+number_v2 = [element for element in range(1, 11)]
+
+print(number_v2)
